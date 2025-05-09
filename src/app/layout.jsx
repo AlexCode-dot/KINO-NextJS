3ce910f11219926e984abdd6127552b52da0b187
Calle <ca.karlsson@hotmail.com>
2025-05-09 17:21:36 +0200

    improved the mobile layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
       </head>
       <body>
