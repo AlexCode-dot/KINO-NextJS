5182080cd9f3b98e98c3a979ff219c363950018b
Deansie <dennis.andersen@lerniastudent.se>
2025-05-10 11:22:59 +0200

    feat(ScreeningCard.jsx): add mockdata for screenings and basic loading state

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import MovieCard from '@/components/MovieCard'
+import ScreeningCards from '@/components/ScreeningCard'
 
 export default function Home() {
   return (
@@ -6,6 +7,10 @@ export default function Home() {
       <h1 className="moviecard__header">Populäraste filmerna just nu</h1>
       <div>
         <MovieCard />
+      </div>
+      <h1 className="screeningcard__header">Visningar för de fem kommande dagarna</h1>
+      <div>
+        <ScreeningCards />
       </div>
     </>
   )
---
+++ src/components/ScreeningCard.jsx
@@ -0,0 +1,145 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+const mockScreeningData = [
+  {
+    Title: 'The Muppets',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '12:00:00',
+    imdbID: 'tt1204342',
+  },
+  {
+    Title: 'Fire Walk With Me',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '17:00:00',
+    imdbID: 'tt0105665',
+  },
+  {
+    Title: 'Min granne Totoro',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '21:00:00',
+    imdbID: 'tt0096283',
+  },
+  {
+    Title: 'Forrest Gump',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '17:00:00',
+    imdbID: 'tt0109830',
+  },
+  {
+    Title: 'Forrest Gump',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '19:00:00',
+    imdbID: 'tt0109830',
+  },
+  {
+    Title: 'Encanto',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '17:00:00',
+    imdbID: 'tt2953050',
+  },
+  {
+    Title: 'Min granne Totoro',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '17:00:00',
+    imdbID: 'tt0096283',
+  },
+  {
+    Title: 'Min granne Totoro',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '19:00:00',
+    imdbID: 'tt0096283',
+  },
+  {
+    Title: 'Training Day',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '12:00:00',
+    imdbID: 'tt0139654',
+  },
+  {
+    Title: 'Forrest Gump',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '17:00:00',
+    imdbID: 'tt0109830',
+  },
+  {
+    Title: 'Encanto',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '21:00:00',
+    imdbID: 'tt2953050',
+  },
+  {
+    Title: 'The Shawshank Redemption',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '12:00:00',
+    imdbID: 'tt0111161',
+  },
+  {
+    Title: 'Forrest Gump',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '21:00:00',
+    imdbID: 'tt0109830',
+  },
+  {
+    Title: 'Isle of Dogs',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '12:00:00',
+    imdbID: 'tt5104604',
+  },
+  {
+    Title: 'The Muppets',
+    Room: 'Stora salongen',
+    Date: '2025-05-14',
+    Time: '19:00:00',
+    imdbID: 'tt1204342',
+  },
+]
+
+export default function ScreeningCards() {
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  //Mock API-delay
+  useEffect(() => {
+    setTimeout(() => {
+      setData(mockScreeningData)
+      setIsLoading(false)
+    }, 1000)
+  }, [])
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error: {error}</div>
+
+  return (
+    <div>
+      <ul className="screeningcard__list">
+        {data.map((item, index) => (
+          <li key={item.imdbID}>
+            <div className="screeningcard__container">
+              <p>{item.Title}</p>
+              <p>{item.Room}</p>
+              <p>{item.Time}</p>
+              <p>{item.Date}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
