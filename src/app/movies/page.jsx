976b2e6db91ea0018e49bea5439f733f982b4742
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-05-18 18:54:40 +0200

    Deleted MoviesList.jsx

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -1,11 +1,13 @@
 'use client'
 
-import MoviesList from '@/components/MoviesList'
-
-export default function Movies() {
+export default function MoviesPage() {
   return (
-    <main>
-      <MoviesList />
-    </main>
+    <div style={{ padding: '2rem' }}>
+      <h1>
+        Den här sidan är under uppbyggnad <br></br>
+        (issue 4.1 layout movies page)
+      </h1>
+      <p>Men ni kan fortfarande se varje film individuellt HÄR: http://localhost:3000/movies/tt1375666 / Ronja</p>
+    </div>
   )
 }
