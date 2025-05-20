a395a467e1e88d93c2114d33eef7fe43388ea3e0
Calle <ca.karlsson@hotmail.com>
2025-05-20 13:40:19 +0200

    Updated styling to login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,9 +5,9 @@ export default function Login() {
         <h1 className="loginPage__Title">Login</h1>
         <section className="loginPage__section">
           <form className="loginPage__form">
-            <h3>Username</h3>
+            <h3 className="loginPage__form_name">Username</h3>
             <input className="loginPage__username" type="text" />
-            <h3>Password</h3>
+            <h3 className="loginPage__form_password">Password</h3>
             <input className="loginPage__password" type="text" />
             <button className="loginPage__button">Login</button>
           </form>
