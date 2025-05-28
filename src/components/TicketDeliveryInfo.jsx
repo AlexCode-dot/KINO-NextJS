b8e4ec096ff4dce9f02631e6105a0b3f52da6807
RobTrb <Robert.trb@gmail.com>
2025-05-28 20:52:05 +0200

    committing just to pull latest

--- src/components/TicketDeliveryInfo.jsx
+++ src/components/TicketDeliveryInfo.jsx
@@ -1,4 +1,4 @@
-export default function TicketDeliveryInfo() {
+export default function TicketDeliveryInfo({ nrOfTickets, setNrOfTickets, customerName, customerEmail }) {
   return (
     <div className="TicketDeliveryInfo__mainContainer">
       <div className="mainContainer__customerDetails">
@@ -6,7 +6,7 @@ export default function TicketDeliveryInfo() {
           <strong>Biljettleverans</strong>
         </a>
         <a>Ange din e-postadress:</a>
-        <input></input>
+        <input placeholder="exempel@mail.com"></input>
         <a>Ange ditt fullständiga namn:</a>
         <input></input>
       </div>
@@ -15,13 +15,22 @@ export default function TicketDeliveryInfo() {
           <strong>Välj antal biljetter:</strong>
         </a>
         <div className="ticketSelector__nrOfTickets">
-          <img src="/img/subtract__btn.webp"></img>
-          <span>0</span>
-          <img src="/img/add__btn.webp"></img>
+          <img
+            src="/img/subtract__btn.webp"
+            onClick={() => {
+              if (nrOfTickets > 0) {
+                setNrOfTickets(nrOfTickets - 1)
+              }
+            }}
+          ></img>
+          <span>{nrOfTickets}</span>
+          <img
+            src="/img/add__btn.webp"
+            onClick={() => {
+              setNrOfTickets(nrOfTickets + 1)
+            }}
+          ></img>
         </div>
-        <a>
-          <strong>Valda platser:</strong>
-        </a>
       </div>
     </div>
   )
