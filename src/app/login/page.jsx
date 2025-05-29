0e8cb615316783e5e402d20bf713cda5ffce5683
Calle <ca.karlsson@hotmail.com>
2025-05-29 11:29:08 +0200

    Added basic login functionality

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,14 +1,46 @@
+'use client'
+import { useState } from 'react'
+
 export default function Login() {
+  const [Username, setUsername] = useState('')
+  const [Password, setPassword] = useState('')
+
   return (
     <main className="loginPage">
       <div className="loginPage__container">
         <h1 className="loginPage__Title">Login</h1>
         <section className="loginPage__section">
-          <form className="loginPage__form">
+          <form
+            className="loginPage__form"
+            onSubmit={(ev) => {
+              ev.preventDefault()
+              const response = fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  Username,
+                  Password,
+                }),
+              })
+              console.log(`Login form submitted ${Username} and ${Password}`)
+            }}
+          >
             <h3 className="loginPage__form_name">Username</h3>
-            <input className="loginPage__username" type="text" />
+            <input
+              className="loginPage__username"
+              type="text"
+              value={Username}
+              onChange={(ev) => setUsername(ev.target.value)}
+            />
             <h3 className="loginPage__form_password">Password</h3>
-            <input className="loginPage__password" type="text" />
+            <input
+              className="loginPage__password"
+              type="password"
+              value={Password}
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
             <button className="loginPage__button">Login</button>
           </form>
         </section>
