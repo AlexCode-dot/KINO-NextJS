da9e82086713a031fa2f25ee5255aa3ce8842cee
Calle <ca.karlsson@hotmail.com>
2025-06-02 11:12:39 +0200

    Added registration page to create accounts

---
+++ src/app/register/page.jsx
@@ -0,0 +1,52 @@
+'use client'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Register() {
+  const [Username, setUsername] = useState('')
+  const [Password, setPassword] = useState('')
+  const [Error, setError] = useState(false)
+  const router = useRouter()
+
+  return (
+    <form
+      className="loginPage__form"
+      onSubmit={async (ev) => {
+        ev.preventDefault()
+        const response = await fetch('/api/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            Username,
+            Password,
+          }),
+        })
+        if (response.ok) {
+          router.push('/login')
+        } else {
+          setError(true)
+        }
+      }}
+    >
+      {Error && <h1 className="loginPage__error_text">Användaren finns redan</h1>}
+      <h1>Registrera konto</h1>
+      <h3 className="loginPage__form_name">Username</h3>
+      <input
+        className="loginPage__username"
+        type="text"
+        value={Username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <h3 className="loginPage__form_password">Password</h3>
+      <input
+        className="loginPage__password"
+        type="password"
+        value={Password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button className="loginPage__button">Registrera</button>
+    </form>
+  )
+}
