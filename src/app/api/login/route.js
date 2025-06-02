94300a040a12053b1d4b0a82ee1f98e4bae2c3c5
Calle <ca.karlsson@hotmail.com>
2025-06-02 11:06:57 +0200

    Refactor login API to verify credentials against database

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,3 +1,5 @@
+import connectDB from '@/lib/db/connectDB'
+import { findUserByUsername } from '@/lib/db/userDbService'
 import { NextResponse } from 'next/server'
 
 export async function POST(request) {
@@ -7,12 +9,17 @@ export async function POST(request) {
   console.log(`${payload.Username} and ${payload.Password} received in the login API`)
   console.log(`Login attempt with username: ${payload.Username} and password: ${payload.Password}`)
 
-  // Verify if username and password match login info
-  if (payload.Username === 'admin' && payload.Password === 'password') {
+  await connectDB()
+  let login = await findUserByUsername(payload.Username)
+
+  console.log(`${login.Username} found in the database`)
+  if (login.Username === payload.Username && login.Password === payload.Password) {
+    console.log(`User ${payload.Username} found in the database`)
     const response = NextResponse.json({ message: 'Login successful' }, { status: 200 })
     response.cookies.set('Username', payload.Username, {})
     return response
   } else {
+    console.log(`Login failed for username: ${payload.Username}`)
     return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
   }
 }
