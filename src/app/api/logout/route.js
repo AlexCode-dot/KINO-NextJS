26b0f50c06d8d842da952ab7615b3d6d67c9eb9b
Calle <ca.karlsson@hotmail.com>
2025-06-02 11:07:31 +0200

    Refactor logout API to await cookie before clearing username

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,7 +1,8 @@
 import { cookies } from 'next/headers'
 
 export async function POST() {
-  cookies().set('Username', '')
+  const storeCookie = await cookies()
+  storeCookie.set('Username', '')
   return new Response(JSON.stringify({ message: 'Logged out' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
