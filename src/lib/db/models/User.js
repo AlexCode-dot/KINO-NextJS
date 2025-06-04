cceac5a4a19e1800d5bc3a28fe11e57f016efa33
Calle <ca.karlsson@hotmail.com>
2025-06-04 10:50:11 +0200

    Added Admin field to user schema with default value set to false

--- src/lib/db/models/User.js
+++ src/lib/db/models/User.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
+  Admin: { type: Boolean, default: false },
 })
 
 export default mongoose.models.User || mongoose.model('User', userSchema)
