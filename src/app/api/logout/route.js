6dea7bb8f04891018aafcddba4d21e4206950cae
Calle <ca.karlsson@hotmail.com>
2025-06-04 10:42:50 +0200

    Added admin cookie clearing on logout

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 export async function POST() {
   const storeCookie = await cookies()
   storeCookie.set('Username', '')
+  storeCookie.set('Admin', '')
   return new Response(JSON.stringify({ message: 'Logged out' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
